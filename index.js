3b4a16e0b985f50e4d00b2fadb78597378d0b329
EmilyDewdrop <turner.ek@gmail.com>
2023-07-05 15:03:04 +0100

    Centred instructions, speech bubble

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ btn.addEventListener('click', clickDolly);
 function clickDolly() {
     var x = document.getElementById("speechBubbleText");
     var container = document.getElementById("instructions");
+    var bubble = document.getElementById("bubble");
 
     if (x.innerHTML === "") {
-      x.innerHTML = "I need to get something off my chest!";
-    } else if (x.innerHTML === "I need to get something off my chest!") {
-      x.innerHTML = "It's this trouble I've been having with this woman Jolene! She keeps trying to steal my man! I've been working 9-5 trying to find her! Can you find her for me?";
-    } else if (x.innerHTML === "It's this trouble I've been having with this woman Jolene! She keeps trying to steal my man! I've been working 9-5 trying to find her! Can you find her for me?") {
+      x.innerHTML = "I need to get something off my chest! It's this trouble I've been having with this woman Jolene! She keeps trying to steal my man! I've been working 9-5 trying to find her! Can you find her for me?";
+      bubble.style.display="block";
+    } else if (x.innerHTML === "I need to get something off my chest! It's this trouble I've been having with this woman Jolene! She keeps trying to steal my man! I've been working 9-5 trying to find her! Can you find her for me?") {
       x.innerHTML = "Her beauty is beyond compare with flaming locks of auburn hair with ivory skin and eyes of emerald green!";
     } else if (x.innerHTML === "Her beauty is beyond compare with flaming locks of auburn hair with ivory skin and eyes of emerald green!") {
       x.innerHTML = "I seem to flow right past her like an island in the stream, but I believe you can catch her! Each time you get her, you'll get 1 point! But make sure you don't accidentally catch anyone else or else you'll lose a point!";
@@ -17,6 +17,7 @@ function clickDolly() {
       x.innerHTML = "And you REALLY don't want to catch my clone! Then you'll lose all your points!";
     } else if (x.innerHTML === "And you REALLY don't want to catch my clone! Then you'll lose all your points!") {
       x.innerHTML = " "
+      bubble.style.display="none";
       container.style.display = "block";
     }
   }
@@ -32,6 +33,14 @@ function hideInstructions() {
     container.style.display = "none";
   }
 
+  //display floating heads
+
+  // function displayHeads() {
+  //   var containers = document.getElementsByClassName("gameHeads");
+  // for (var i = 0; i < containers.length; i++) {
+  //   containers[i].style.display = "block";
+  // }
+  // }
   //starts the movement of all the floating heads
 
   function startMovingDC() {
